d0e9c44ea6f742bf1eb2e646d6a0b12f95efd30b
M <41164583+ifmcjthenknczny@users.noreply.github.com>
2022-03-12 23:48:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// $("body").onload(function() {
+//     $(".header").animate({
+//         left: '250px',
+//         opacity: '0.5',
+//         height: '150px',
+//         width: '150px'
+//     });
+// });
